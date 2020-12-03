f7850a05557253f86a6d55b42992c26b5d8a3d27
haneenalnahwi <haneenalnahwi@gmail.com>
2020-12-03 21:56:42 +0200

    Editing thumbnails function to convert all image formats

--- Py_Homework_4.py
+++ Py_Homework_4.py
@@ -7,12 +7,16 @@ def grey(image):
     return grey_img
 
 def thumbnails(directory):
-    for img_address in glob.glob(directory + '\\*.jpg'):
-        img = Image.open(img_address)
-        img.thumbnail((128, 128), Image.ANTIALIAS)
-        img_name = img_address.replace(directory + '\\', '')
+    img_format = ['jpg', 'png', 'gif']
+    img_format_O = ['JPEG', 'PNG', 'GIF']
+    for frmt in img_format:
+        for img_address in glob.glob(directory+'\\*.'+frmt):
+            img = Image.open(img_address)
+            img.thumbnail((128, 128), Image.ANTIALIAS)
+            img_name = img_address.replace(directory + '\\', '')
 
-        if img_name[0:2] != 'T_':
-            img_address_new = img_address.replace(directory + '\\', directory + '\\T_')
-            im.save(img_address_new, "JPEG")
-
+            if img_name[0:2] != 'T_':
+                img_address_new = img_address.replace(directory + '\\', directory + '\\T_')
+                for i in range(3):
+                    if frmt == img_format[i]:
+                        img.save(img_address_new, img_format_O[i])
