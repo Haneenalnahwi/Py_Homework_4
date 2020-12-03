9a5d62577cdd6e92200eae78cd83b0e5c8fc9036
haneenalnahwi <haneenalnahwi@gmail.com>
2020-12-04 01:23:08 +0200

    Adding an option for current directory

--- Py_HW_4_code.py
+++ Py_HW_4_code.py
@@ -1,3 +1,12 @@
-import Py_HW_4_Model
+import IP_Model as ip
 
+img = 'SierraSunset.Over.Deluxe.JPG'
+im = ip.ImageProcessing(img)
+grey_img = im.grey()
+grey_img.show()
+print('\n')
+print('The Histogram of the image is: ', '\n', im.histogram())
+print('\n')
+dir = 'current'
+ip.thumbnails(dir)
 
