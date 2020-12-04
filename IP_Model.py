a0eacbeeededb6871ebe7b2838880c433b3cea35
haneenalnahwi <haneenalnahwi@gmail.com>
2020-12-04 13:52:50 +0200

    commenting to explain the code

--- IP_Model.py
+++ IP_Model.py
@@ -2,32 +2,32 @@ from PIL import Image
 import glob
 import numpy as np
 
-class ImageProcessing:
-    def __init__(self, image):
+class ImageProcessing:          #defining a class that takes an image
+    def __init__(self, image):                  #initializing by opening the image
         self.img = Image.open(image)
 
-    def show(self):
+    def show(self):                             #defining a function to show the image from pilow package
         self.img.show()
 
-    def size(self):
+    def size(self):                             #defining a function to find the image size from pilow package
         self.img.size
 
-    def grey(self):
+    def grey(self):                             #defining a function to convert the image color to grey
         grey_img = self.img.convert("L")
         return grey_img
 
-    def histogram(self):
-        grey_img = self.grey()
-        grey_img_n = grey_img.point(lambda i: int(i / 28))
+    def histogram(self):                         #defining a function to find 10 bin histogram for the image
+        grey_img = self.grey()                   #converting to grey using the predefined grey function
+        grey_img_n = grey_img.point(lambda i: int(i / 28))        #coverting from 0-255 range to 0-10 range
         size = grey_img_n.size
         length = size[0]
         width = size[1]
-        pixel = np.zeros((length, width))
+        pixel = np.zeros((length, width))        #defining an array to store pixels of the image
         for i in range(length):
             for j in range(width):
                 pixel[i][j] = grey_img_n.getpixel((i, j))
         histo = {}
-        for i in range(length):
+        for i in range(length):                  #calculating the count of each grey level
             for j in range(width):
                 if pixel[i][j] not in histo.keys():
                     histo[pixel[i][j]] = 1
@@ -35,19 +35,19 @@ class ImageProcessing:
                     histo[pixel[i][j]] = histo[pixel[i][j]] + 1
         return histo
 
-def thumbnails(directory):
+def thumbnails(directory):                              #defining a function that converts each image in a directory to thumbnail image
     img_format = ['jpg', 'png', 'gif']
     img_format_O = ['JPEG', 'PNG', 'GIF']
     directory_ = directory + '\\'
-    if directory == 'current':
+    if directory == 'current':                          #if the directory is the current directory where the code exists
         for frmt in img_format:
-            for img_name in glob.glob('*.' + frmt):
+            for img_name in glob.glob('*.' + frmt):      #using glob package to go to directory path
                 img = Image.open(img_name)
-                img.thumbnail((100, 100), Image.ANTIALIAS)
-                if img_name[0:2] != 'T_':
+                img.thumbnail((100, 100), Image.ANTIALIAS)  #using thumbnail function to convert image to thumbnail with maximum size 100*100
+                if img_name[0:2] != 'T_':                #ensuring that the image is not coverted yet
                     for i in range(3):
                         if frmt == img_format[i]:
-                            img.save('T_' + img_name, img_format_O[i])
+                            img.save('T_' + img_name, img_format_O[i])   #saving thumbnail image with T_ prefix
     else:
         for frmt in img_format:
             for img_address in glob.glob(directory_+'*.'+frmt):
